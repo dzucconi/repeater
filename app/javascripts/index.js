e97b785250aa192d8f47f04abb85bf25c1dad564
dzucconi <mail@damonzucconi.com>
2017-05-20 10:42:01 -0400

    Cleans up

--- app/javascripts/index.js
+++ app/javascripts/index.js
@@ -2,6 +2,7 @@ import parameters from 'queryparams';
 import fps from 'frame-interval';
 
 import randomColor from './lib/random_color';
+import times from './lib/times';
 
 window.parameters = parameters;
 
@@ -12,6 +13,7 @@ const DOM = {
 const CONFIG = parameters({
   size: 2,
   gap: 2,
+  axes: ['x', 'y'],
 });
 
 const init = () => {
@@ -23,22 +25,24 @@ const init = () => {
   const nY = Math.floor(height / (CONFIG.size * CONFIG.gap));
   const nX = Math.floor(width / (CONFIG.size * CONFIG.gap));
 
-  const yMapping = Array(nY).fill(undefined).map((_, i) => CONFIG.size * i * CONFIG.gap);
-  const xMapping = Array(nX).fill(undefined).map((_, i) => CONFIG.size * i * CONFIG.gap);
+  const map = i => CONFIG.size * i * CONFIG.gap;
+
+  const yMapping = times(nY)(map);
+  const xMapping = times(nX)(map);
 
   fps(requestAnimationFrame)(60, (i) => {
-    const y = yMapping[i % yMapping.length];
+    if (CONFIG.axes.includes('y')) {
+      const y = yMapping[i % yMapping.length];
+      ctx.fillStyle = randomColor();
+      ctx.fillRect(0, y, width, CONFIG.size);
+    }
 
-    ctx.fillStyle = randomColor();
-    ctx.fillRect(0, y, width, CONFIG.size);
-
-    const x = xMapping[i % xMapping.length];
-
-    ctx.fillStyle = randomColor();
-    ctx.fillRect(x, 0, CONFIG.size, height);
+    if (CONFIG.axes.includes('x')) {
+      const x = xMapping[i % xMapping.length];
+      ctx.fillStyle = randomColor();
+      ctx.fillRect(x, 0, CONFIG.size, height);
+    }
   })();
 };
 
-export default () => {
-  init();
-};
+export default init;
